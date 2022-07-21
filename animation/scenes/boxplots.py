cbc0c9a7a56d7a1c203f2843e548eace774c4913
Adil Mohiuddin <amohi9046@gmail.com>
2022-07-21 08:20:58 -0700

    add sorted scene & refactor a bit

--- animation/scenes/boxplots.py
+++ animation/scenes/boxplots.py
@@ -1,25 +1,23 @@
 from manim import *
 
-def get_axes_label(ax, labels):
-    label_group = VGroup()
-    for i in range(len(labels)):
-        label = Text(labels[i], font_size=24)
-        label.rotate(PI / 2)
-        label.next_to(ax.y_axis.n2p(i + 1), LEFT)
-        label_group.add(label)
-    return label_group
 
-
-def get_axes(labels, x_range):
+def get_axes(label, x_range):
     ax = Axes(
         x_range=x_range,
-        y_range=[0, len(labels) + 1, 1],
+        y_range=[0,  2, 1],
         x_axis_config={"numbers_to_include": np.arange(
             x_range[0], x_range[1] + 1, x_range[2])},
         tips=False
     )
-    labels = get_axes_label(ax, labels)
-    return VGroup(ax, labels)
+
+    label = Text(label, font_size=24)
+    label.rotate(PI / 2)
+    label.next_to(ax.y_axis.n2p(1), LEFT)
+
+    axes_label = ax.get_axis_labels(x_label="\mu", y_label="")
+
+
+    return VGroup(ax, label, axes_label)
 
 
 def get_boxplot(ax, data, y, height):
@@ -39,7 +37,6 @@ def get_boxplot(ax, data, y, height):
     upper_line = Line(upper_q_point.get_center(), max_point.get_center())
     median_line = Line(median_point.get_center() + UP * height,
                        median_point.get_center() + DOWN * height)
-
     outliers = []
     for x in data['outliers']:
         point = Dot([ax.c2p(x, y)]).get_center()
@@ -56,20 +53,25 @@ def get_boxplot(ax, data, y, height):
 
 
 def draw_boxplot(scene, boxplot):
+    outliers = VGroup()
     for mobject in boxplot:
         if mobject.name == "Line" or mobject.name == "Rectangle":
             scene.play(Create(mobject))
-        elif mobject.name == "Text" or mobject.name == "Dot":
+        elif mobject.name == "Dot":
             scene.play(GrowFromCenter(mobject), run_time=.5)
+        elif mobject.name == "Text":
+            outliers.add(mobject)
+
+    scene.play(Write(outliers))
 
 
-def replace_graph(scene, old_ax, old_plot, label, new_range, new_data):
-    new_ax = get_axes([label], new_range)
+def replace_graph(scene, old_graph, label, new_range, new_data):
+    new_ax = get_axes(label, new_range)
     new_plot = get_boxplot(new_ax, new_data, 1, 0.75)
-    scene.play(ReplacementTransform(old_ax, new_ax),
-               FadeOut(old_plot, shift=RIGHT * 3), FadeIn(new_plot, shift=RIGHT * 3))
+    scene.play(ReplacementTransform(old_graph[0], new_ax),
+               FadeOut(old_graph[1], shift=RIGHT * 3), FadeIn(new_plot, shift=RIGHT * 3))
     scene.wait()
-    return new_ax, new_plot
+    return VGroup(new_ax, new_plot)
 
 
 class UnsortedBoxplot(Scene):
@@ -129,20 +131,131 @@ class UnsortedBoxplot(Scene):
             "outliers": [80.4496, 80.527, 81.1568, 81.2081, 81.4157, 82.4489]
         }
 
-        bubble_ax = get_axes(['Bubble'], [800, 1700, 100])
+        bubble_ax = get_axes('Bubble', [800, 1700, 100])
         bubble_ax_plot = get_boxplot(bubble_ax, bubble_sort, 1, 0.75)
+        bubble_graph = VGroup(bubble_ax, bubble_ax_plot)
         self.play(Write(bubble_ax))
         draw_boxplot(self, bubble_ax_plot)
 
-        selection_ax, selection_plot = replace_graph(self, bubble_ax, bubble_ax_plot,
-                                                     'Selection', [575, 595, 5], selection_sort)
-        insertion_ax, insertion_plot = replace_graph(self, selection_ax, selection_plot,
-                                                     'Insertion', [215, 230, 5], insertion_sort)
-        heap_ax, heap_plot = replace_graph(self, insertion_ax, insertion_plot,
-                                           'Heap', [95, 99, 1], heap_sort)
-        merge_ax, merge_plot = replace_graph(self, heap_ax, heap_plot,
-                                             'Merge', [124, 130, 1], merge_sort)
-        quick_ax, quick_plot = replace_graph(self, merge_ax, merge_plot,
-                                             'Quick', [80, 83, 1], quick_sort)
+        selection_graph = replace_graph(self, bubble_graph, 'Selection',
+                                        [575, 595, 5], selection_sort)
+        insertion_graph = replace_graph(self, selection_graph, 'Insertion',
+                                        [215, 230, 5], insertion_sort)
+        heap_graph = replace_graph(self, insertion_graph, 'Heap',
+                                   [95, 99, 1], heap_sort)
+        merge_graph = replace_graph(self, heap_graph, 'Merge',
+                                    [124, 130, 1], merge_sort)
+        quick_graph = replace_graph(self, merge_graph, 'Quick',
+                                    [80, 83, 1], quick_sort)
 
-        self.play(FadeOut(quick_ax, quick_plot))
+        self.play(FadeOut(quick_graph))
+        self.wait()
+
+
+class Transition(Scene):
+    def construct(self):
+        self.play(FadeOut(self.mobjects))
+        self.wait()
+
+
+class SortedBoxplot(Scene):
+    def construct(self):
+        selection_sort = {
+            "low_q": 496.319,
+            "upper_q": 497.249,
+            "median": 496.74,
+            "min": 495.082,
+            "max": 498.595,
+            "outliers": [492.869, 494.384, 494.386, 494.744, 494.799, 494.909,
+                         494.915, 498.652, 499.225, 499.399, 499.712, 500.848,
+                         502.655],
+        }
+
+        quick_sort = {
+            "low_q": 281.8725,
+            "upper_q": 285.0225,
+            "median": 283.417,
+            "min": 278.253,
+            "max": 289.022,
+            "outliers": [273.941, 275.993, 276.94, 277.077]
+        }
+
+        merge_sort = {
+            "low_q": 58.449,
+            "upper_q": 59.1349,
+            "median": 58.6485,
+            "min": 57.8202,
+            "max": 60.1374,
+            "outliers": [61.3913]
+        }
+
+        heap_sort = {
+            "low_q":  21.93445,
+            "upper_q": 22.59225,
+            "median": 22.23135,
+            "min": 21.003,
+            "max": 23.3924,
+            "outliers": [20.9035, 20.9132, 23.6025, 23.6372, 23.6696,
+                         23.7356, 23.9435, 24.0476, 24.3529, 24.9226,
+                         26.0656]
+        }
+
+        bubble_sort = {
+            "low_q": 1.21296,
+            "upper_q": 1.215785,
+            "median": 1.21402,
+            "min": 1.21161,
+            "max": 1.21984,
+            "outliers": [1.22268, 1.22307, 1.22479, 1.23174, 1.23338, 1.234,
+                         1.23891, 1.24996, 1.26328, 1.26476, 1.50799]
+        }
+
+        insertion_sort = {
+            "low_q": 0.780005,
+            "upper_q": 0.780475,
+            "median": 0.7802465,
+            "min": 0.779396,
+            "max": 0.78118,
+            "outliers": [0.77854, 0.779258, 0.781543, 0.781783, 0.781854,
+                         0.782062, 0.782089, 0.782471, 0.782652, 0.829324,
+                         0.949045, 0.980058]
+        }
+
+        selection_ax = get_axes('Selection', [490, 504, 2])
+        selection_plot = get_boxplot(selection_ax, selection_sort, 1, 0.75)
+        selection_graph = VGroup(selection_ax, selection_plot)
+        self.play(Write(selection_ax))
+        draw_boxplot(self, selection_plot)
+
+        quick_graph = replace_graph(self, selection_graph, 'Quick',
+                                    [272, 290, 2], quick_sort)
+
+        merge_graph = replace_graph(self, quick_graph, 'Merge',
+                                    [56, 62, 1], merge_sort)
+
+        heap_graph = replace_graph(self, merge_graph, 'Heap',
+                                   [20, 28, 2], heap_sort)
+
+        bubble_graph = replace_graph(self, heap_graph, 'Bubble',
+                                     [1.2, 1.5, 0.1], bubble_sort)
+
+        bubble_zoomed_ax = get_axes('Bubble', [1.2, 1.28, 0.02])
+        bubble_zoomed_plot = get_boxplot(
+            bubble_zoomed_ax, bubble_sort, 1, 0.75)
+        bubble_zoomed_graph = VGroup(bubble_zoomed_ax, bubble_zoomed_plot)
+        self.play(ReplacementTransform(bubble_graph, bubble_zoomed_graph))
+        self.wait()
+
+        insertion_graph = replace_graph(self, bubble_zoomed_graph, 'Insertion',
+                                        [0.7, 1, 0.1], insertion_sort)
+
+        insertion_zoomed_ax = get_axes('Insertion', [0.779, 0.782, 0.001])
+        insertion_zoomed_plot = get_boxplot(
+            insertion_zoomed_ax, insertion_sort, 1, 0.75)
+        insertion_zoomed_graph = VGroup(
+            insertion_zoomed_ax, insertion_zoomed_plot)
+        self.play(ReplacementTransform(
+            insertion_graph, insertion_zoomed_graph))
+        self.wait()
+        self.play(FadeOut(insertion_zoomed_graph))
+        self.wait()
